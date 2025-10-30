7165b7d74ecc72e899d68c4cc734934007ee6df5
arr <kuash2448@gmail.com>
2025-10-30 18:47:45 +0530

    Use environment variable for API URL

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -14,7 +14,8 @@ const Contact = () => {
 
 
     try {
-    const res = await fetch(`https://abhiash-3.onrender.com/api/contact`, {
+const res = await fetch(`https://abhiash-3.onrender.com/api/contact`
+, {
 
         method: "POST",
         headers: { "Content-Type": "application/json" },
